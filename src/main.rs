4efcc48d437c970082d6733a63ee20ab5e3a5412
cadolphs <clemens.adolphs@gmail.com>
2022-11-28 13:51:49 -0800

    Add skeleton for day 04 to test spr.
    Testing out a cli tool for stacked diffs so I need a commit to see how it looks like.
    
    Test Plan: cargo build and cargo test should run without fail.
    
    Reviewers: 
    
    Pull Request: https://github.com/cadolphs/aoc_2021_rust/pull/1

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 pub mod day01;
 pub mod day02;
 pub mod day03;
+pub mod day04;
 pub mod inputs;
 
 fn main() {
     day01::run_day_01();
     day02::run_day_02();
     day03::run_day_03();
+    day04::run_day_04();
 }
