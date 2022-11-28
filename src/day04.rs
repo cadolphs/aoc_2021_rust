395ea546ac7e0100754e0c9409d5f04e4772aecd
Clemens Adolphs <clemens.adolphs@1qbit.com>
2022-11-28 13:48:24 -0800

    Forgot the source file
    Created using spr 1.3.4

---
+++ src/day04.rs
@@ -0,0 +1,13 @@
+use std::{error::Error, str::FromStr};
+
+use crate::inputs::read_file_to_list;
+
+pub fn run_day_04() {
+    let filename = "data/day04.txt";
+    println!("Well. Nothing here yet.");
+}
+
+
+#[cfg(test)]
+mod tests {
+}
